52b656bd04ff27cdee2f1e498d0e3e2673b22088
Aasish Pappu <aasishkp@yahoo-inc.com>
2016-04-09 20:13:10 -0400

    Added Apache LICENSE file

--- src/main/java/com/yahoo/semsearch/fastlinking/CoherentEntityLinker.java
+++ src/main/java/com/yahoo/semsearch/fastlinking/CoherentEntityLinker.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Stream;
 
 /**
  * Created by aasishkp on 12/15/15.
@@ -147,6 +148,11 @@ public class CoherentEntityLinker {
             new String[]{"Abedin", "Hillary Clinton", "Clinton", "Elijah Cummings", "Abedin", "Clinton"};
 
         CoherentEntityLinker coherentEntityLinker = new CoherentEntityLinker(args[0], args[1], args[2], args[3]);
+
+        String[] orgs = new String[] {"Apple", "Google", "Apple Computer"};
+        List<String> orgsList = Arrays.asList(orgs);
+        Stream<List<String>> wikiLinksToLink = orgsList.stream().map(org -> coherentEntityLinker.getWikiLinks(org));
+
 
         int lenSurfaceStrings = surfaceStrings.length;
         ArrayList<String> nBestList[] = new ArrayList[]{new ArrayList<String>(Arrays.asList(A0)),
