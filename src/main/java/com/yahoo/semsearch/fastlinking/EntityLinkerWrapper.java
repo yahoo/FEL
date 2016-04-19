76acf3850082409c5575c9611e42e871382cf2ce
Aasish Pappu <aasishkp@yahoo-inc.com>
2016-04-19 09:41:43 -0400

    1. Added instructions to run coherent entity linker to README 2. Updated license header

--- src/main/java/com/yahoo/semsearch/fastlinking/EntityLinkerWrapper.java
+++ src/main/java/com/yahoo/semsearch/fastlinking/EntityLinkerWrapper.java
@@ -1,10 +1,10 @@
 /**
- Copyright 2016, Yahoo! Inc.
+ Copyright 2016, Yahoo Inc.
  Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
  **/
 
 /**
- Copyright 2016, Yahoo! Inc.
+ Copyright 2016, Yahoo Inc.
  Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
  **/
 package com.yahoo.semsearch.fastlinking;
